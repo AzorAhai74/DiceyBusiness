2565c33170a1fa21d152bb623be64c92fa10b153
Your Name <you@your-domain.com>
2020-03-01 23:34:27 -0600

    setup

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 window.addEventListener('DOMContentLoaded', function() {
+
+    const container = document.createElement('main');
+    container.className = 'container';
+
+    document.body.appendChild(container);
+
+    let counter = 1;
     
-})
+    class Dice {
+        constructor() {
+            this.div = document.createElement('div');
+            this.value = document.createTextNode(counter);
+            this.div.className = 'dice';
+            this.div.id = counter;
+            container.appendChild(this.div);
+            this.div.addEventListener('mouseover', () => this.div.appendChild(this.value));
+            this.div.addEventListener('mouseout', () => this.div.removeChild(this.value));
+        }
+    }
+});
