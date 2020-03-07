1ece97d68c4404ef01b985dbe7bb67394df9c690
Your Name <you@your-domain.com>
2020-03-07 14:02:50 -0600

    border around card

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
+    let newDie = 1;
 
-    const container = document.createElement('main');
-    container.className = 'container';
-
-    document.body.appendChild(container);
-
-    let counter = 1;
+    let newDieDiv = document.createElement('div');
+    document.body.appendChild(newDieDiv);
     
-    class Dice {
-        constructor() {
-            this.div = document.createElement('div');
-            this.value = document.createTextNode(counter);
-            this.div.className = 'dice';
-            this.div.id = counter;
-            container.appendChild(this.div);
-            this.div.addEventListener('mouseover', () => this.div.appendChild(this.value));
-            this.div.addEventListener('mouseout', () => this.div.removeChild(this.value));
-        }
-    }
 });
