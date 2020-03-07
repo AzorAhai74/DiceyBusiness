ad999861cd11c0ef3a9433c596c20e8a3b92285b
Your Name <you@your-domain.com>
2020-03-07 14:58:50 -0600

    progess

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 window.addEventListener('DOMContentLoaded', function() {
+    const button1 = document.getElementById('genDieBtn');
+    
     let newDie = 1;
 
-    let newDieDiv = document.createElement('div');
-    document.body.appendChild(newDieDiv);
+    
+    button1.addEventListener('click', function() {
+        let div = document.createElement('div');
+        div.className = 'die';
+        let div1 = document.getElementsByClassName('die');
+        div.class = div1;
+        div.id = (newDie);
+
+        document.body.appendChild(div);
+        div.classList.add('die');
+        div.style.cssFloat = 'right';
+        div.style.textAlign = 'center';
+        div.style.height = '100px';
+        div.style.lineHeight = '100px';
+        div.style.webkitTextFillColor = 'white';
+
+        newDie++;
+    });
+    
+
     
 });
